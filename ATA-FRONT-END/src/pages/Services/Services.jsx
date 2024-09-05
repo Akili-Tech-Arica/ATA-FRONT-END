2b0cd6486d177cd3e0f25670a38b2ec14c1588d5
manaseschege <manaseschege0gmail.com>
2024-09-05 09:48:29 +0300

    modified components to tailwind

--- ATA-FRONT-END/src/pages/Services/Services.jsx
+++ ATA-FRONT-END/src/pages/Services/Services.jsx
@@ -1,45 +1,37 @@
-import React from 'react'
-import "./Services.css"
-import coding from "../../assets/courses_coding.jpg"
+import React from 'react';
+import coding from "../../assets/courses_coding.jpg";
+
 function Services() {
   return (
-    <div id="services" className="services">
-      <h1 className='services_head'>Our Services</h1>
-      <h4>
-        We provide accessible, transformative digital training that's is
-        designed for Africa
+    <div id="services" className="bg-gray-100 py-16 px-4">
+      <h1 className="text-center font-bold text-3xl text-indigo-700 mb-6 font-serif">Our Services</h1>
+      <h4 className="text-center text-xl font-light mb-12 text-gray-700">
+        We provide accessible, transformative digital training that's designed for Africa
       </h4>
-      <div className="services_cards">
-        <div className="services_info">
-          <div className="service_img">
-            <img src={coding} alt="" />
-            <div className="red_circle_service"></div>
-            <div className="yellow_triangle_services"></div>
-          </div>
-          <h3>Affordable techie team training</h3>
-        </div>
 
-        <div className="services_info">
-          <div className="service_img">
-            <img src={coding} alt="" />
-            <div className="red_circle_service"></div>
-            <div className="yellow_triangle_services"></div>
-          </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
+        {Array(3).fill(0).map((_, idx) => (
+          <div
+            key={idx}
+            className="relative bg-white p-6 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105 transform transition-all duration-300 ease-in-out w-full max-w-xs cursor-pointer"
+          >
+            <div className="relative mb-6">
+              <img
+                src={coding}
+                alt="coding service"
+                className="w-full h-48 object-cover rounded-xl border-b-8 border-yellow-500"
+              />
+              <div className="absolute top-0 left-0 w-16 h-16 bg-red-500 rounded-full flex items-center justify-center transform -translate-x-4 -translate-y-4 shadow-lg transition-all duration-300 hover:scale-110">
+              </div>
+              <div className="absolute bottom-0 right-0 w-10 h-10 bg-yellow-500 transform rotate-45 translate-x-3 translate-y-3"></div>
+            </div>
 
-          <h3>Affordable techie team training</h3>
-        </div>
-
-        <div className="services_info">
-          <div className="service_img">
-            <img src={coding} alt="" />
-            <div className="red_circle_service"></div>
-            <div className="yellow_triangle_services"></div>
+            <h3 className="text-indigo-700 font-semibold text-lg text-center">Affordable techie team training</h3>
           </div>
-          <h3>Affordable techie team training</h3>
-        </div>
+        ))}
       </div>
     </div>
   );
 }
 
-export default Services
+export default Services;

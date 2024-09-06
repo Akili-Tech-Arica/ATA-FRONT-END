7fceec457f71873d9e538add62ba238f61df56b8
oluoch <joseph.oluoch@tracom.co.ke>
2024-09-06 08:22:31 +0300

    Refactor: code refactored to tailwindUI
    our project originally built on vanilla html and css re-configured to be worked on using tailwind UI and CSS

--- ATA-FRONT-END/src/pages/Motivation/Motivation.jsx
+++ ATA-FRONT-END/src/pages/Motivation/Motivation.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Motivation.module.css';
+import sideImg from "../../assets/Asset_5-removebg-preview-l.png";
+import sideLeft from "../../assets/Asset_5-removebg-preview.png";
 
 function Motivation() {
   const videoId = '2-VKC8g2u1Y'; // Replace with video ID if needed
@@ -7,6 +9,16 @@ function Motivation() {
 
   return (
     <div id="motivation" className={styles.container}>
+      <img
+        className="absolute bottom-4 left-0 w-20 h-40 object-cover opacity-75"
+        src={sideImg}
+        alt="Left Decoration"
+      />
+      <img
+        className="absolute top-4 right-0 w-28 h-40 object-cover opacity-75"
+        src={sideLeft}
+        alt="Right Decoration"
+      />
       <div className={styles.videoContainer}>
         <iframe
           width="560"
